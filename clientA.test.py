077a346bbe39b889bbce39cc01411860e633fa10
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2023-05-27 07:48:29 +0000

    tests

---
+++ clientA.test.py
@@ -0,0 +1,33 @@
+import unittest
+import requests
+
+
+class TestAliceClient(unittest.TestCase):
+
+    def test_sync(self):
+        i = 0
+        while i < 10:
+            b = 1
+            b_json = {'bA': b}
+            b_rst = requests.post('http://localhost:5000/AliceBit', json=b_json )
+            print(b_rst)
+            b_rst = b_rst.json()
+            print(b_rst)
+            print(f"i={i}")
+            i += 1
+            self.assertEqual(int(b_rst['result']), b)
+        
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
